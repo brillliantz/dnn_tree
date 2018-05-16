2bb0417155663718e4c63b4d02241285c6518aea
BrillliantZ <brillliantz@outlook.com>
2018-05-17 02:57:49 +0800

    add train/test mode

--- my_dataset.py
+++ my_dataset.py
@@ -16,7 +16,8 @@ class FutureTickDataset(Dataset):
 
     """
     def __init__(self, backward_window=240, forward_window=60, transform=None,
-                 cut_len=0, lite_version=True):
+                 cut_len=0, lite_version=True, train=True):
+        self.train_mode = train
         self.dir = 'Data/future'
         if lite_version:
             self.data_fp = os.path.join(self.dir,
@@ -28,7 +29,6 @@ class FutureTickDataset(Dataset):
                                         'rb1701_201608-201611_AddFeature.hd5')
             self.mask_fp = os.path.join(self.dir,
                                         'mask_all.hd5')
-
 
         self.cut_len = cut_len
         self.transform = transform
@@ -82,26 +82,40 @@ class FutureTickDataset(Dataset):
         # 目标预测变量
         self._df_raw.loc[:, 'y'] = self._df_raw['mid'].pct_change(self.forward_window).shift(-self.forward_window)
 
+        # TODO: std
+        roll = self._df_raw.rolling(window=self.backward_window, axis=0)
+        self._df_raw = (self._df_raw - roll.mean()) / roll.std()
+
         self.df = self._df_raw.loc[self._mask].dropna()
 
         # X, Y都是np.ndarray
         self.x = self.df[X_COLS].values
         self.y = self.df['y'].values.reshape([-1, 1])
 
-        # TODO
-        self.x = (self.x - self.x.mean(axis=0)) / self.x.std(axis=0)
-        self.y = (self.y - self.y.mean(axis=0)) / self.y.std(axis=0)
-
         self.x = self.x.astype(np.float32)
         self.y = self.y.astype(np.float32)
+
+        train_ratio = 0.7
+        train_len = int(len(self.x) * train_ratio)
+        if self.train_mode:
+            self.x = self.x[: train_len]
+            self.y = self.y[: train_len]
+        else:
+            self.x = self.x[train_len:]
+            self.y = self.y[train_len:]
 
         # [time_window, n_feature] to [n_feature, time_window]
         # because torch.nn.Conv inputs are of shape [batch, n_channels, sample_shape]
         self.x = np.swapaxes(self.x, 0, 1)
         self.y = np.swapaxes(self.y, 0, 1)
+        
+        import gc
+        del self._df_raw
+        del self.df
+        gc.collect()
 
     def __len__(self):
-        return len(self.df) - self.backward_window
+        return self.x.shape[1] - self.backward_window
 
     def __getitem__(self, idx):
         sample_x = self.x[:, idx: idx + self.backward_window]
@@ -113,9 +127,50 @@ class FutureTickDataset(Dataset):
         return sample_x, sample_y
 
 
-def get_future_loader(batch_size, cut_len, lite_version=True):
-    ds = FutureTickDataset(240, 60, cut_len=cut_len, lite_version=lite_version)
-    ds_test = FutureTickDataset(240, 60, cut_len=cut_len)
+class FutureBarDataset(Dataset):
+    """
+    China Future Tick Data.
+
+    """
+    def __init__(self, train=True, cut_len=0):
+        # self.dir = 'Data/future'
+        if train:
+            x = np.load('Data/futures-historical-data/rb_X_train_1501-1701.npy')
+            y = np.load('Data/futures-historical-data/rb_Y_train_1501-1701.npy')
+        else:
+            x = np.load('Data/futures-historical-data/rb_X_test_1501-1701.npy')
+            y = np.load('Data/futures-historical-data/rb_Y_test_1501-1701.npy')
+
+        # test with minimal data size
+        if cut_len:
+            x = x[:cut_len]
+            y = y[:cut_len]
+
+        y = y[:, 0]  # dicard time step dimension (which is 1)
+
+        # from keras.utils.np_utils import to_categorical
+        # y = to_categorical(y, num_classes=3)
+        y = y.reshape([-1])
+        y = y + 1
+
+        self.x = np.asanyarray(x, dtype=np.float32)
+        self.y = np.asarray(y, dtype=np.int64)
+        self.x = np.swapaxes(self.x, 1, 2)
+        # self.y = np.swapaxes(self.y, 1, 2)
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        sample_x = self.x[idx]
+        sample_y = self.y[idx]
+
+        return sample_x, sample_y
+
+
+def get_future_bar_classification_data(batch_size, cut_len):
+    ds = FutureBarDataset(train=True, cut_len=cut_len)
+    ds_test = FutureBarDataset(train=False, cut_len=0)
     print("Train dataset len: {:d}\n"
           "Test dataset len: {:d}".format(len(ds), len(ds_test)))
 
@@ -127,6 +182,25 @@ def get_future_loader(batch_size, cut_len, lite_version=True):
     print("Iterations needed per epoch: {:d}".format(itr_per_epoch))
 
     trainloader = DataLoader(ds, batch_size=batch_size, shuffle=False,)
+    testloader = DataLoader(ds_test, batch_size=batch_size, shuffle=False,)
+
+    return trainloader, testloader
+
+
+def get_future_loader(batch_size, cut_len, lite_version=True):
+    ds_train = FutureTickDataset(224, 60, cut_len=cut_len, train=True, lite_version=lite_version)
+    ds_test = FutureTickDataset(224, 60, cut_len=cut_len, train=False, lite_version=lite_version)
+    print("Train dataset len: {:d}\n"
+          "Test dataset len: {:d}".format(len(ds_train), len(ds_test)))
+
+    y_abs = np.abs(ds_train.y)
+    print("Y mean = {:.3e}, Y median = {:.3e}".format(np.mean(y_abs), np.median(y_abs)))
+
+    ds_len = len(ds_train)
+    itr_per_epoch = ds_len // batch_size
+    print("Iterations needed per epoch: {:d}".format(itr_per_epoch))
+
+    trainloader = DataLoader(ds_train, batch_size=batch_size, shuffle=False,)
     testloader = DataLoader(ds_test, batch_size=batch_size, shuffle=False,)
 
     return trainloader, testloader
@@ -209,9 +283,12 @@ def load_imagenet(data_dir, batch_size, n_workers):
 
 
 if __name__ == "__main__":
+    # a, b = get_future_bar_classification_data(16)
+
     # test_dataset_for_loop()
     from utils import time_it
     time_it(
-        #test_dataset_for_loop
-        test_dataset_loader
+        # test_dataset_for_loop
+        # test_dataset_loader
+        get_cifar_10, 10, shuffle=True
     )
